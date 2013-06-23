4f4aa3aa4d527e732c4b0c38fbdb1c6ec17015d7
Rajat Khandelwal <rajatgupta59@gmail.com>
2013-06-23 16:44:31 +0530

    so that gunicorn can serve static files

--- OnlineExam/urls.py
+++ OnlineExam/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,5 @@ urlpatterns = patterns('',
     url(r'^auth/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/exam/home'}),
     url(r'^exam/', include('exam.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
